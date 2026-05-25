6b2d74dd1971a9a8b99693b5ed70a0fbf0507632
lalindux <lalindux@gmail.com>
2026-05-25 08:43:33 +0530

    Updated code in C file

--- main.c
+++ main.c
@@ -3,6 +3,45 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    float number1 = 0;
+    float number2 = 0;
+    char symbol ;
+    float result = 0;
+
+    printf("Enter your first Number :");
+    scanf("%f",&number1);
+
+    printf("Enter your Second Number :");
+    scanf("%f",&number2);
+
+    printf("Choose operation (+, -, *, /):");
+    scanf(" %c",&symbol);
+
+    printf("\n");
+
+    if (symbol == '+'){
+
+        result = number1+ number2;
+        printf("Your Final Result is :%.1f",result);
+    }else if (symbol == '-'){
+        result = number1 - number2;
+        printf("Your Final Result is :%.1f",result);
+    }else if (symbol == '*'){
+        result = number1 * number2;
+        printf("Your Final Result is :%.1f",result);
+    }else if (symbol == '/'){
+        result = number1 / number2;
+        printf("Your Final Result is :%.1f",result);
+    }else {
+        printf("Invalid Input");
+    }
+    printf("\n");
+
+
+
     return 0;
+
+
+
+
 }
